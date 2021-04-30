862f29a3c99e6d8d3b0d6a7476e039ccb0b92ed7
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2021-04-30 16:58:09 -0400

    implement layouts

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var expressLayouts = require('express-ejs-layouts');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,6 +11,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
+app.set('layout', './layouts/main'); 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -18,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressLayouts);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
