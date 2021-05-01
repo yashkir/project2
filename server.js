b77a866bd2e2d991c32b1633e855e6727d2b39f1
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2021-04-30 22:41:07 -0400

    set up session middleware

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var expressLayouts = require('express-ejs-layouts');
+var session = require('express-session');
 
 require('dotenv').config();
 require('./config/database');
@@ -23,6 +24,11 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
