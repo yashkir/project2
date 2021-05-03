a828ac5898e75346e5cee2c94f217e79193e26a3
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2021-05-03 13:02:30 -0400

    remove old log

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,6 @@
 var passport = require('passport');
 var User = require('../models/user');
 
-console.log(User.createStrategy());
 passport.use(User.createStrategy());
 
 passport.serializeUser(User.serializeUser());
